73251e72d86e5510d3d4aa9c5388c845d4f25f35
Yitzchok Sabel <yitzchok@d7mtg.com>
2025-03-23 16:59:01 -0400

    Initial Commit

---
+++ PaleoHebrew/PaleoHebrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaleoHebrewApp.swift
+//  PaleoHebrew
+//
+//  Created by Yitzchok Sabel on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PaleoHebrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaleoHebrewTests/PaleoHebrewTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaleoHebrewTests.swift
+//  PaleoHebrewTests
+//
+//  Created by Yitzchok Sabel on 3/23/25.
+//
+
+import Testing
+@testable import PaleoHebrew
+
+struct PaleoHebrewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
